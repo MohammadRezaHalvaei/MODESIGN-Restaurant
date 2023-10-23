dc11f76cf23f2706abdbf35a8ebd6518f0e9f869
MohammadReza Halvaei <mamalijvj@gmail.com>
2023-10-23 07:07:28 +0330

    commented static params

--- app/chefs/[name]/page.tsx
+++ app/chefs/[name]/page.tsx
@@ -2,19 +2,19 @@ import ChefsDetails from "@/components/Chefs/Single/ChefsDetails";
 import Video from "@/components/Chefs/Single/Video";
 import Reservation from "@/components/Footer/Reservation";
 
-async function getPosts(): Promise<FetchDataType[]> {
-  const req = await fetch(process.env.NEXT_PUBLIC_API_URL + `/api/chefs`);
+// async function getPosts(): Promise<FetchDataType[]> {
+//   const req = await fetch(process.env.NEXT_PUBLIC_API_URL + `/api/chefs`);
 
-  return await req.json();
-}
+//   return await req.json();
+// }
 
-export async function generateStaticParams() {
-  const chefs = await getPosts();
+// export async function generateStaticParams() {
+//   const chefs = await getPosts();
 
-  return chefs?.map((chef) => ({
-    name: chef.name,
-  }));
-}
+//   return chefs?.map((chef) => ({
+//     name: chef.name,
+//   }));
+// }
 
 export default function ChefsName({
   params: { name },
